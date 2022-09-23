33fe888886a4bacbbe640a32ba8ff7061f1dad87
Kirill <budanovkirilldmitrievich@gmail.com>
2022-09-23 19:43:12 +0300

    main app

--- your_text/auth_app/forms.py
+++ your_text/auth_app/forms.py
@@ -11,3 +11,16 @@ class UserLoginForm(AuthenticationForm):
         super(UserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field_name
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field_name
--- your_text/your_text/urls.py
+++ your_text/your_text/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('auth/', include('auth_app.urls', namespace='auth')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
