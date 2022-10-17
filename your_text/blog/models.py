c9eb0fb0f57a00039b3cdfd68e42ae9a21d35e9a
Kirill <budanovkirilldmitrievich@gmail.com>
2022-10-17 23:23:17 +0300

    blog start

---
+++ your_text/blog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from auth_app.models import User
+
+
+class UserPost(models.Model):
+    title = models.CharField(
+        max_length=64,
+    )
+
+    image = models.ImageField(
+        upload_to='blogs_images',
+        blank=True)
+
+    content = models.TextField(
+        blank=True,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    like_count = models.DecimalField(
+        max_digits=8,
+        decimal_places=0,
+        default=0
+    )
+
+    comment_count = models.DecimalField(
+        max_digits=8,
+        decimal_places=0,
+        default=0
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+class Comment(models.Model):
+    text = models.TextField(
+        blank=False,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
